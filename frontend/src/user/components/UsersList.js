39feb9d13abaa98a39578e14dfc75e1742336cd7
viknhat <viknhat@gmail.com>
2022-10-12 20:12:58 +0530

    FE

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,27 +1,28 @@
-import UserItem from "./UserItem";
-import "./UsersList.css";
+import React from 'react';
 
-const UsersList = (props) => {
-  if (props.items.length == 0) {
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = props => {
+  if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No Users Found.</h2>
+        <h2>No users found.</h2>
       </div>
     );
   }
+
   return (
-    <ul className='users-list'>
-      {props.items.map((user) => {
-        return (
-          <UserItem
-            key={user.id}
-            id={user.id}
-            image={user.image}
-            name={user.name}
-            resumeCount={user.resumes}
-          />
-        );
-      })}
+    <ul className="users-list">
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
   );
 };
