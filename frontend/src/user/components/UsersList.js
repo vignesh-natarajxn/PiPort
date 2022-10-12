be4690e6cfb5fee1eb3fb8e5b870d3e704fd1e98
viknhat <viknhat@gmail.com>
2022-10-12 20:01:11 +0530

    frotnend

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,7 +1,29 @@
+import UserItem from "./UserItem";
 import "./UsersList.css";
 
 const UsersList = (props) => {
-  return <></>;
+  if (props.items.length == 0) {
+    return (
+      <div className="center">
+        <h2>No Users Found.</h2>
+      </div>
+    );
+  }
+  return (
+    <ul>
+      {props.items.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            resumeCount={user.resumes}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 export default UsersList;
