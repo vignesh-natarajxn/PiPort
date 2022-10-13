2618d1e7958ffae601d9bcf033e473bc043cfab8
viknhat <viknhat@gmail.com>
2022-10-13 16:35:35 +0530

    users

---
+++ frontend/src/resumes/components/ResumeList.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import ResumeItem from "./ResumeItem";
+import "./ResumeList.css";
+
+const ResumeList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found. Maybe create one?</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <ResumeItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ResumeList;
