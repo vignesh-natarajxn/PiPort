c1f912b4e2b9aba82f87704ae2ef823079ccce1b
viknhat <viknhat@gmail.com>
2022-10-14 18:37:18 +0530

    new resume

--- frontend/src/resumes/pages/NewResume.js
+++ frontend/src/resumes/pages/NewResume.js
@@ -1,5 +1,14 @@
+import React from 'react';
+
+import Input from '../../shared/components/FormElements/Input';
+import './NewResume.css';
+
 const NewResume = () => {
-  return <h2>New Resume</h2>;
+  return (
+    <form className="resume-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
 export default NewResume;
