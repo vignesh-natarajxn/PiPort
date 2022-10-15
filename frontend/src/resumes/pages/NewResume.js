36ecf0c6c8dfc544116555ac6a85d8913571299f
viknhat <viknhat@gmail.com>
2022-10-15 18:58:38 +0530

    validator

--- frontend/src/resumes/pages/NewResume.js
+++ frontend/src/resumes/pages/NewResume.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import "./NewResume.css";
 
 const NewResume = () => {
@@ -10,7 +11,7 @@ const NewResume = () => {
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
       />{" "}
     </form>
