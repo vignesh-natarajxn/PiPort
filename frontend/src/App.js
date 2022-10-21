ad560b5e5c9883ebd49ba3f0f1cb5ba4ffeeea42
viknhat <viknhat@gmail.com>
2022-10-21 21:41:07 +0530

    added login app wide responses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -11,45 +12,48 @@ import UserResumes from "./resumes/pages/UserResumes";
 import UpdateResume from "./resumes/pages/UpdateResume";
 import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-
-import "./App.css";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/:userId/resumes" exact>
-            <UserResumes />
-          </Route>
-          <Route path="/resumes/new" exact>
-            <NewResume />
-          </Route>
-          <Route path="/resumes/:resumeId">
-            <UpdateResume />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/:userId/resumes" exact>
+              <UserResumes />
+            </Route>
+            <Route path="/resumes/new" exact>
+              <NewResume />
+            </Route>
+            <Route path="/resumes/:resumeId">
+              <UpdateResume />
+            </Route>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
 export default App;
-
-{
-  /* <div
-      style={{
-        backgroundColor: "#292929",
-        // width: "100px",
-        // height: "100px",
-      }}
-    ></div> */
-}
