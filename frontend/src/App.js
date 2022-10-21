a8cc6c957106ea2f10a22cdc8a0d47a49e98673f
viknhat <viknhat@gmail.com>
2022-10-21 21:50:07 +0530

    conditional permissions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
-} from "react-router-dom";
+  Switch
+} from 'react-router-dom';
 
-import Users from "./user/pages/Users";
-import NewResume from "./resumes/pages/NewResume";
-import UserResumes from "./resumes/pages/UserResumes";
-import UpdateResume from "./resumes/pages/UpdateResume";
-import Auth from "./user/pages/Auth";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import { AuthContext } from "./shared/context/auth-context";
+import Users from './user/pages/Users';
+import NewResume from './resumes/pages/NewResume';
+import UserResumes from './resumes/pages/UserResumes';
+import UpdateResume from './resumes/pages/UpdateResume';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,32 +25,50 @@ const App = () => {
     setIsLoggedIn(false);
   }, []);
 
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/resumes" exact>
+          <UserResumes />
+        </Route>
+        <Route path="/resumes/new" exact>
+          <NewResume />
+        </Route>
+        <Route path="/resumes/:resumeId">
+          <UpdateResume />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/resumes" exact>
+          <UserResumes />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
   return (
     <AuthContext.Provider
       value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
     >
       <Router>
         <MainNavigation />
-        <main>
-          <Switch>
-            <Route path="/" exact>
-              <Users />
-            </Route>
-            <Route path="/:userId/resumes" exact>
-              <UserResumes />
-            </Route>
-            <Route path="/resumes/new" exact>
-              <NewResume />
-            </Route>
-            <Route path="/resumes/:resumeId">
-              <UpdateResume />
-            </Route>
-            <Route path="/auth">
-              <Auth />
-            </Route>
-            <Redirect to="/" />
-          </Switch>
-        </main>
+        <main>{routes}</main>
       </Router>
     </AuthContext.Provider>
   );
