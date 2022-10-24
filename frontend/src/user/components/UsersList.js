14f63aaa9b489c957e0f0dfb68fe0cefb5713077
viknhat <viknhat@gmail.com>
2022-10-24 22:43:26 +0530

    user structure changes

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import UserItem from './UserItem';
-import './UsersList.css';
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 
-const UsersList = props => {
+import "./UserItem.css";
+
+const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2 style={{ color: 'white'}}>No users found.</h2>
+        <h2 style={{ color: "white" }}>No users found.</h2>
       </div>
     );
   }
 
   return (
     <ul className="users-list">
-      {props.items.map(user => (
-        <UserItem
-          key={user.id}
-          id={user.id}
-          image={user.image}
-          name={user.name}
-          portfolioCount={user.portfolios}
-        />
+      {props.items.map((user) => (
+        <li className="user-item">
+          <Card className="user-item__content">
+            <Link to={`/${user.id}/portfolios`}>
+              <div className="user-item__image">
+                <Avatar image={user.image} alt={user.name} />
+              </div>
+              <div className="user-item__info">
+                <h2>{user.name}</h2>
+                <h3>
+                  {user.portfolioCount}{" "}
+                  {user.portfolioCount === 1 ? "Portfolio" : "Portfolios"}
+                </h3>
+              </div>
+            </Link>
+          </Card>
+        </li>
       ))}
     </ul>
   );
