5ada242e4038ac22f309f4ec9dc141d335840cc7
viknhat <viknhat@gmail.com>
2022-10-24 23:00:35 +0530

    route to portf

--- frontend/src/portfolios/components/PortfolioItem.js
+++ frontend/src/portfolios/components/PortfolioItem.js
@@ -1,20 +1,21 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 
-import Card from '../../shared/components/UIElements/Card';
-import Button from '../../shared/components/FormElements/Button';
-import Modal from '../../shared/components/UIElements/Modal';
-import Map from '../../shared/components/UIElements/Map';
-import { AuthContext } from '../../shared/context/auth-context';
-import './PortfolioItem.css';
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
+// import Map from '../../shared/components/UIElements/Map';
+import { AuthContext } from "../../shared/context/auth-context";
+import "./PortfolioItem.css";
 
-const PortfolioItem = props => {
+const PortfolioItem = (props) => {
   const auth = useContext(AuthContext);
-  const [showMap, setShowMap] = useState(false);
+  // const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
-  const openMapHandler = () => setShowMap(true);
+  // const openMapHandler = () => setShowMap(true);
 
-  const closeMapHandler = () => setShowMap(false);
+  // const closeMapHandler = () => setShowMap(false);
 
   const showDeleteWarningHandler = () => {
     setShowConfirmModal(true);
@@ -26,12 +27,12 @@ const PortfolioItem = props => {
 
   const confirmDeleteHandler = () => {
     setShowConfirmModal(false);
-    console.log('DELETING...');
+    console.log("DELETING...");
   };
 
   return (
-    <React.Fragment>
-      <Modal
+    <Link style={{textDecoration: 'none'}} to="/">
+        {/* <Modal
         show={showMap}
         onCancel={closeMapHandler}
         header={props.address}
@@ -42,55 +43,55 @@ const PortfolioItem = props => {
         <div className="map-container">
           <Map center={props.coordinates} zoom={16} />
         </div>
-      </Modal>
-      <Modal
-        show={showConfirmModal}
-        onCancel={cancelDeleteHandler}
-        header="Are you sure?"
-        footerClass="portfolio-item__modal-actions"
-        footer={
-          <React.Fragment>
-            <Button inverse onClick={cancelDeleteHandler}>
-              CANCEL
-            </Button>
-            <Button danger onClick={confirmDeleteHandler}>
-              DELETE
-            </Button>
-          </React.Fragment>
-        }
-      >
-        <p>
-          Do you want to proceed and delete this portfolio? Please note that it
-          can't be undone thereafter.
-        </p>
-      </Modal>
-      <li className="portfolio-item">
-        <Card className="portfolio-item__content">
-          <div className="portfolio-item__image">
-            <img src={props.image} alt={props.title} />
-          </div>
-          <div className="portfolio-item__info">
-            <h2>{props.title}</h2>
-            <h3>{props.address}</h3>
-            <p>{props.description}</p>
-          </div>
-          <div className="portfolio-item__actions">
-            <Button inverse onClick={openMapHandler}>
-              VIEW ON MAP
-            </Button>
-            {auth.isLoggedIn && (
-              <Button to={`/portfolios/${props.id}`}>EDIT</Button>
-            )}
-
-            {auth.isLoggedIn && (
-              <Button danger onClick={showDeleteWarningHandler}>
+      </Modal> */}
+        <Modal
+          show={showConfirmModal}
+          onCancel={cancelDeleteHandler}
+          header="Are you sure?"
+          footerClass="portfolio-item__modal-actions"
+          footer={
+            <React.Fragment>
+              <Button inverse onClick={cancelDeleteHandler}>
+                CANCEL
+              </Button>
+              <Button danger onClick={confirmDeleteHandler}>
                 DELETE
               </Button>
-            )}
-          </div>
-        </Card>
-      </li>
-    </React.Fragment>
+            </React.Fragment>
+          }
+        >
+          <p>
+            Do you want to permanently delete this portfolio? Please note that
+            this cannot be undone.
+          </p>
+        </Modal>
+        <li className="portfolio-item">
+          <Card className="portfolio-item__content">
+            <div className="portfolio-item__image">
+              <img src={props.image} alt={props.title} />
+            </div>
+            <div className="portfolio-item__info">
+              <h2>{props.title}</h2>
+              <h3>{props.address}</h3>
+              <p>{props.description}</p>
+            </div>
+            <div className="portfolio-item__actions">
+              {/* <Button inverse onClick={openMapHandler}>
+              OPEN
+            </Button> */}
+              {auth.isLoggedIn && (
+                <Button to={`/portfolios/${props.id}`}>EDIT</Button>
+              )}
+
+              {auth.isLoggedIn && (
+                <Button danger onClick={showDeleteWarningHandler}>
+                  DELETE
+                </Button>
+              )}
+            </div>
+          </Card>
+        </li>
+    </Link>
   );
 };
 
