ccc05b5ddddd8a4688b655ef2b67b1476bd98e90
viknhat <viknhat@gmail.com>
2022-11-01 22:13:50 +0530

    users controllers

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const uuid = require("uuid/v4");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const portfoliosRoutes = require("./routes/portfolios-routes");
-const HttpError = require("./models/http-error");
+const portfoliosRoutes = require('./routes/portfolios-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/portfolios", portfoliosRoutes);
+app.use('/api/portfolios', portfoliosRoutes); // => /api/portfolios...
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
@@ -20,48 +21,8 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
-// CORS Headers => Required for cross-origin/ cross-server communication
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
-
-app.get("/products", (req, res, next) => {
-  res.status(200).json({ products: DUMMY_PRODUCTS });
-});
-
-app.post("/product", (req, res, next) => {
-  const { title, price } = req.body;
-
-  if (!title || title.trim().length === 0 || !price || price <= 0) {
-    return res.status(422).json({
-      message: "Invalid input, please enter a valid title and price.",
-    });
-  }
-
-  const createdProduct = {
-    id: uuid(),
-    title,
-    price,
-  };
-
-  DUMMY_PRODUCTS.push(createdProduct);
-
-  res
-    .status(201)
-    .json({ message: "Created new product.", product: createdProduct });
-});
-
-app.listen(5000); // start Node + Express server on port 5000
+app.listen(5000);
