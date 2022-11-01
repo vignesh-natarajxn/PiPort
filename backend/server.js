653e4989811fea942ad87d84d1f1cfbfee1a8ab5
viknhat <viknhat@gmail.com>
2022-11-01 20:58:21 +0530

    error handling

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use("/api/portfolios", placesRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
 const DUMMY_PRODUCTS = []; // not a database, just some in-memory storage for now
 
 app.use(bodyParser.json());
