530f134724a4626fcc0e718219748a28fdf98dc2
viknhat <viknhat@gmail.com>
2022-11-01 21:17:31 +0530

    controllers + some error handling

---
+++ backend/controllers/portfolios-controllers.js
@@ -0,0 +1,221 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PORTFOLIOS = [
+  {
+    id: "u1r1",
+    creator: "u1",
+    title: "Software Development Engineer",
+    description: "I like to type stuff.",
+    imageUrl:
+      "https://cutewallpaper.org/22/minimal-programming-wallpapers/930213660.jpg",
+    components: [
+      {
+        title: "Professional Experience",
+        description: "",
+        image: "",
+        components: [
+          {
+            title: "Experience 1",
+            date: "01/01/2001 - 01/01/2010",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+          {
+            title: "Experience 2",
+            date: "02/02/2002 - 02/02/2020",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+        ],
+      },
+      {
+        title: "Projects",
+        description: "",
+        image: "",
+        components: [
+          {
+            title: "Experience 1",
+            date: "01/01/2001 - 01/01/2010",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+          {
+            title: "Experience 2",
+            date: "02/02/2002 - 02/02/2020",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "u1r2",
+    creator: "u1",
+    title: "Embedded Firmware Developer",
+    description: "I give life to computers.",
+    imageUrl: "https://cdn.wallpapersafari.com/23/71/Ow4QZ5.png",
+    components: [
+      {
+        title: "Professional Experience",
+        description: "",
+        image: "",
+        components: [
+          {
+            title: "Experience 1",
+            date: "01/01/2001 - 01/01/2010",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+          {
+            title: "Experience 2",
+            date: "02/02/2002 - 02/02/2020",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+        ],
+      },
+      {
+        title: "Projects",
+        description: "",
+        image: "",
+        components: [
+          {
+            title: "Experience 1",
+            date: "01/01/2001 - 01/01/2010",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+          {
+            title: "Experience 2",
+            date: "02/02/2002 - 02/02/2020",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "u2r1",
+    creator: "u2",
+    title: "Chief Executive Officer - Earth",
+    description: "I am your creator.",
+    imageUrl:
+      "https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/web_first_images_release.png?itok=g21NrdRw",
+    components: [
+      {
+        title: "Professional Experience",
+        description: "",
+        image: "",
+        components: [
+          {
+            title: "Experience 1",
+            date: "01/01/2001 - 01/01/2010",
+            description: "",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+          {
+            title: "Experience 2",
+            date: "02/02/2002 - 02/02/2020",
+            description: "",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+        ],
+      },
+      {
+        title: "Projects",
+        description: "",
+        image: "",
+        components: [
+          {
+            title: "Experience 1",
+            date: "01/01/2001 - 01/01/2010",
+            description: "",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+          {
+            title: "Experience 2",
+            date: "02/02/2002 - 02/02/2020",
+            description: "",
+            image:
+              "https://cdn.fansshare.com/photograph/backgroundhd/black-pattern-hd-wallpaper-pattern-1589253890.jpg",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const getPortfolioById = (req, res, next) => {
+  const portfolioId = req.params.pid; // { pid: 'p1' }
+
+  const portfolio = DUMMY_PORTFOLIOS.find((p) => {
+    return p.id === portfolioId;
+  });
+
+  if (!portfolio) {
+    throw new HttpError("Could not find a portfolio for the provided id.", 404);
+  }
+
+  res.json({ portfolio }); // => { portfolio } => { portfolio: portfolio }
+};
+
+// function getPortfolioById() { ... }
+// const getPortfolioById = function() { ... }
+
+const getPortfolioByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const portfolio = DUMMY_PORTFOLIOS.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!portfolio) {
+    return next(
+      new HttpError("Could not find a portfolio for the provided user id.", 404)
+    );
+  }
+
+  res.json({ portfolio });
+};
+
+const createPortfolio = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  // const title = req.body.title;
+  const createdPortfolio = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+
+  DUMMY_PLACES.push(createdPortfolio); //unshift(createdPortfolio)
+
+  res.status(201).json({place: createdPortfolio});
+};
+
+exports.getPortfolioById = getPortfolioById;
+exports.getPortfolioByUserId = getPortfolioByUserId;
+exports.createPortfolio = createPortfolio;
