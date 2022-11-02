2b120305a6acd8c7a096e9ac38b974c7fe472d10
viknhat <viknhat@gmail.com>
2022-11-02 20:26:19 +0530

    mongodb (mongoose)

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const portfoliosRoutes = require('./routes/portfolios-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const portfoliosRoutes = require("./routes/portfolios-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/portfolios', portfoliosRoutes); // => /api/portfolios...
-app.use('/api/users', usersRoutes);
+app.use("/api/portfolios", portfoliosRoutes); // => /api/portfolios...
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -21,8 +22,17 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://vygneys:miscrits@cluster0.waoqkza.mongodb.net/portfolios?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
