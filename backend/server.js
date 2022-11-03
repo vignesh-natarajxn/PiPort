698e564d63c98a5b0936fff533ed6ad02e741621
viknhat <viknhat@gmail.com>
2022-11-03 22:08:13 +0530

    initial connection between frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/api/portfolios", portfoliosRoutes); // => /api/portfolios...
 app.use("/api/users", usersRoutes);
 
@@ -28,7 +39,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://vygneys:miscrits@cluster0.waoqkza.mongodb.net/portfolios?retryWrites=true&w=majority"
+    "mongodb+srv://vygneys:miscrits@cluster0.waoqkza.mongodb.net/piport?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(5000);
